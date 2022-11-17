83079d4d2c7b6b662763802bfd6670d8df90bebc
rishi <mrrishi646@gmail.com>
2022-11-17 21:01:01 +0530

    intial commit

---
+++ ReactJS/my-app/src/components/about.js
@@ -0,0 +1,13 @@
+import Topbar from "./topbar/topbar";
+import Header from "./header";
+
+function About(){
+    return (
+        <>
+                {/* <Topbar/> */}
+        <Header/>
+        </>
+
+     )
+}
+  export default About
